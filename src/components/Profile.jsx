b9df0b412c036aa59bb6305e84bb64a369b000dc
YarL03 <yaroslav.labetsky@yandex.ru>
2022-02-26 18:00:59 +0800

    some edits

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Profile = () => {
     return (
         <div className="profile">
-            <div className="user">
+            <div className="userAva">
                 <img src="https://sun9-45.userapi.com/impg/pdhdc93j8Ib2TqV8rjKE644cHAzrpbN6BprpSg/lLtLcMfaXN8.jpg?size=1280x960&quality=95&sign=1c61305bbc5cd4a882295f78385a37af&type=album"/>
             </div>
 
